cfd3d61c4665f7a8954d60d33864801bc02becb1
Rohit Debnath <rohit.kumardebnath.cse28@heritageit.edu.in>
2025-06-11 21:50:28 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [count,setCount] =useState(5);
+  
+//let count =5;
+const addValue=()=>{
+  if(count>=20){
+    count=20;
+  }else if(count<20){
+    setCount(++count);
+  }
+}
+const remValue=()=>{
+  if(count<0){
+    count=0
+  }else if(count>0){
+    setCount(--count);
+  }
+ 
+}
+
+
+  return (
+    <>
+     
+
+      <h1>code aur chai</h1>
+      <h2>Counter : {count} </h2>
+      <button
+       onClick={addValue}>
+         add value: {count}</button>
+      <br/>
+      <button
+      onClick={remValue}>remove value :{count} </button>
+      
+      
+    </>
+  )
+}
+
+export default App
